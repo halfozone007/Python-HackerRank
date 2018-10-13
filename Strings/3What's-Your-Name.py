5eb60a95bb8be15c3c9fe04248a09aa925d0a7ce
Gaurav Ramola <git.gramola007@gmail.com>
2018-10-13 16:44:07 +0530

    Python-HackerRank-Strings What's your Name

---
+++ Strings/3What's-Your-Name.py
@@ -0,0 +1,18 @@
+
+
+
+def print_full_name(a, b):
+    resultstr = "Hello "+ a + " " + b+ "!"+ " You just delved into python."
+    print(resultstr)
+
+def main():
+    first_name = input()
+    last_name = input()
+
+    print_full_name(first_name, last_name)
+
+
+
+if __name__ == '__main__':
+    main()
+    
