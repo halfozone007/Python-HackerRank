d4c855b176583ceb7a9fd00efefd6e7a5722e157
Gaurav Ramola <git.gramola007@gmail.com>
2018-10-13 13:46:03 +0530

    Strings- SwapCase

---
+++ Strings/1SwapCase.py
@@ -0,0 +1,19 @@
+
+
+def SwapCase(str):
+    swapCasedStr = str.swapcase()
+    return swapCasedStr
+    
+
+
+def main():
+    inputStr = input()
+    result = SwapCase(inputStr)
+    print(result)
+
+
+
+
+if __name__ == '__main__':
+    main()
+    
